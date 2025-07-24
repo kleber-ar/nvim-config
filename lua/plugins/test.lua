cca18a4ee1fa39e963404510f6ac17204354e6d1
kleber-ar <kleber-ar@hotmail.com>
2025-07-24 09:22:50 -0300

    att test.lua

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,9 +1,46 @@
 return {
   {
     'nvim-neotest/neotest',
-    dependencies = { 'nvim-neotest/neotest-jest', 'marilari88/neotest-vitest' },
+    dependencies = {
+      'haydenmeade/neotest-jest',
+      'marilari88/neotest-vitest',
+    },
+    keys = {
+      {
+        '<leader>tl',
+        function()
+          require('neotest').run.run_last()
+        end,
+        desc = 'Run Last Test',
+      },
+      {
+        '<leader>tL',
+        function()
+          require('neotest').run.run_last({ strategy = 'dap' })
+        end,
+        desc = 'Debug Last Test',
+      },
+
+      {
+        '<leader>tw',
+        function()
+          vim.cmd('split | terminal npm run test')
+        end,
+        desc = 'Run npm test in horizontal terminal',
+      },
+    },
     opts = function(_, opts)
-      table.insert(opts.adapters, require('neotest-jest'))
+      table.insert(
+        opts.adapters,
+        require('neotest-jest')({
+          jestCommand = 'npm test --',
+          jestConfigFile = 'custom.jest.config.ts',
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
       table.insert(opts.adapters, require('neotest-vitest'))
     end,
   },
